c17c428b572507d0e3fd3bddcf5c4a20f8f0d7d9
melovemingming <mingming@mingming.email>
2019-12-26 09:36:35 +0800

    day-07

--- day-07/src/com/ming14/Test01.java
+++ day-07/src/com/ming14/Test01.java
@@ -13,15 +13,13 @@ public class Test01 {
             }
         }.start();
 
-        new Runnable(){
-            @Override
-            public void run() {
-                int sum = 0;
-                for(int i = 0; i < 500; i++){
-                    sum += i;
-                }
-                System.out.println(sum);
-            }
-        }.run();
+
+        Thread thread = new Thread(new  Runnable() {
+                    @Override
+                    public void run() {
+                        System.out.println("333");
+                    }
+                }, "ming");
+        thread.start();
     }
 }
