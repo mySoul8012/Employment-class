8d117c34ef7502fa6fe33816ebd62d6d21e2ca26
melovemingming <mingming@mingming.email>
2020-01-07 18:10:32 +0800

    day-15

--- day-15/src/main/java/com/ming8/Test01.java
+++ day-15/src/main/java/com/ming8/Test01.java
@@ -9,10 +9,12 @@ public class Test01 {
         // 获取结果
         String res = scanner.nextLine();
         // 输出第一个正则
-        String zhenze1 = "[0-9]";
+        String zhenze1 = "^[0-9]*";
         // 输出匹配结果
         System.out.println(res.matches(zhenze1));
         // 定义第二位是 3,5,6,7,8中的一位
-        String zhenze02 = ""
+        String zhenze02 = "^1[3578][0-9]+";
+        // 输出匹配结果
+        System.out.println(res.matches(zhenze02));
     }
 }
